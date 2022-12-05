d7bf9b1849536d7c0be23cddad33dcfca6f086df
“akashmane3938” <maneakash3938@gmail.com>
2022-12-05 03:20:11 -0800

    Linked List Programs

---
+++ LinkedList_Solutions/LinkedList_Solutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedList_Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List Programs");
+        }
+    }
+}
